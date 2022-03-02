17da03d99552c1d972b0fec09b67162a34990185
Rustykik <valentin.dubr@gmail.com>
2022-03-02 15:57:07 +0300

    fixed seg and closing wrong discriptors

--- src/pipexv2/pipex.c
+++ src/pipexv2/pipex.c
@@ -6,7 +6,7 @@
 /*   By: rusty <rusty@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 11:34:16 by rusty             #+#    #+#             */
-/*   Updated: 2022/02/28 22:18:19 by rusty            ###   ########.fr       */
+/*   Updated: 2022/02/28 22:42:12 by rusty            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,6 @@ void	exec_cmd(t_cmd *cmd, t_shell *shell)
 		full_cmd = find_path(ft_split(get_env(shell->env, "PATH"), ':'), cmd->cmd_name);
 	// if (check_not_dir(full_cmd)
 	//	 exit(126); // minishell: full cmd: Is a directory
-	ft_printf("full_cmd -[%s]\n", full_cmd);
 	if (access(full_cmd, F_OK) == -1)
 		exit (127); // recheck exit status command does not found
 	if (access(full_cmd, X_OK) == -1)
