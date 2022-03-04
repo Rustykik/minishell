5dad0fbe8e2bea6ca54c13d25f4159bdfc4ebde5
Manhack Jacqualine <majacqua@oa-j3.msk.21-school.ru>
2022-03-04 18:27:25 +0300

    Exit

--- src/shell/init_commands.c
+++ src/shell/init_commands.c
@@ -3,14 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   init_commands.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rusty <rusty@student.42.fr>                +#+  +:+       +#+        */
+/*   By: majacqua <majacqua@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 10:52:38 by rusty             #+#    #+#             */
-/*   Updated: 2022/03/04 05:59:04 by rusty            ###   ########.fr       */
+/*   Updated: 2022/03/04 18:26:41 by majacqua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+int	ft_isstrdigit(char *str)
+{
+	if (!str)
+		return (1);
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+void	check_exit_str(t_shell *shell)
+{
+	int		i;
+	char	**sp;
+
+	i = 0;
+	// if (shell->cmds != 1)
+	// 	return ;
+	sp = ft_split(shell->cmds_arr[0]->input, ' ');
+	while (sp[i])
+		i++;
+	if (!ft_strncmp(sp[0], "exit", ft_strlen("exit")))
+	{
+		shell->exit = 1;
+		if (i > 1)
+			shell->exit_status = ft_atoi(sp[1]);
+		if (i > 1 && !ft_isstrdigit(sp[1]))
+		{
+			err_return_one("Exit", ERR_NE_ARGS);
+			shell->exit_status = 255;
+		}
+		else if (i > 2)
+		{
+			shell->exit = 0;
+			err_return_one("Exit", ERR_MN_ARGS);
+		}
+	}
+}
 
 t_cmd	*init_cmd(char *cmd)
 {
@@ -47,7 +89,7 @@ int	init_commands(t_shell *shell, char **parsed)
 	i = -1;
 	while (++i < shell->cmds_count)
 		shell->cmds_arr[i] = init_cmd(parsed[i]);
-	// check_exit(); ADD _CHECK EXIT
+	check_exit_str(shell);
 	if (nord_stream(shell) || put_redir_cmds(shell))
 		return (1);
 	return (0);
